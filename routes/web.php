1943c0e0dd816984ffee2c5bd9bb8bb527bcdeb7
amlxv <amirulfcso@gmail.com>
2023-05-29 22:18:43 +0800

    imp: root route condition

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/',
+    fn () => Auth::check() ? view('dashboard') : view('layouts.guest')
+);
