45c4bd93f5ed84d0a1d26721d0125a5d56c7dd80
amlxv <amirulfcso@gmail.com>
2023-05-30 23:18:16 +0800

    fix: tag not closed

--- resources/views/admin/doctor/index.blade.php
+++ resources/views/admin/doctor/index.blade.php
@@ -75,7 +75,7 @@
                                         class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">Active</span>
                                     </td>
                                     <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit
+                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                     </td>
                                 </tr>
                             @endforeach
