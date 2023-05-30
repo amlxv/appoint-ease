f2a335a3cadc06c74561cf23a191eb3297f0d822
amlxv <amirulfcso@gmail.com>
2023-05-30 23:15:49 +0800

    imp: user relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,60 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * Manage users respective to their role
+     *
+     * @return void
+     */
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        /**
+         * Create if not exist.
+         *
+         * @param User $user
+         * @return void
+         */
+        static::created(function (User $user): void {
+            if ($user->isDoctor()) {
+                $user->doctor()->create();
+            } else if ($user->isPatient()) {
+                $user->patient()->create();
+            }
+        });
+
+        /**
+         * Create if not exist, delete if no longer.
+         *
+         * @param User $user
+         * @return void
+         */
+        static::updated(function (User $user): void {
+            if (!$user->isDoctor()) {
+                $user->doctor()->delete();
+            } elseif (!$user->doctor()->exists()) {
+                $user->doctor()->create();
+            }
+
+            if (!$user->isPatient()) {
+                $user->patient()->delete();
+            } elseif (!$user->patient()->exists()) {
+                $user->patient()->create();
+            }
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +67,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -42,4 +89,67 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var string[] $attributes
+     */
+    protected $attributes = [
+        'role' => 'patient',
+    ];
+
+    /**
+     * Check the users role
+     *
+     * @param string $role
+     * @return bool
+     */
+    public function hasRole(string $role): bool
+    {
+        return $this->role === $role;
+    }
+
+    /**
+     * Check if the users is a admin
+     *
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->hasRole('admin');
+    }
+
+    /**
+     * Check if the users is a doctor
+     *
+     * @return bool
+     */
+    public function isDoctor(): bool
+    {
+        return $this->hasRole('doctor');
+    }
+
+    /**
+     * Check if the users is a patient
+     *
+     * @return bool
+     */
+    public function isPatient(): bool
+    {
+        return $this->hasRole('patient');
+    }
+
+    /**
+     * Relationship for patient
+     */
+    public function patient(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Patient::class);
+    }
+
+    public function doctor(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Doctor::class);
+    }
 }
