9757fd25d4d14683c74f8ebe856b28f3254a77b3
amlxv <amirulfcso@gmail.com>
2023-05-31 12:45:38 +0800

    imp: add status

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,5 +15,20 @@ class Doctor extends Model
         'specialization',
         'qualification',
         'experience',
+        'status',
     ];
+
+    /**
+     * Ensure the status returns a boolean value
+     *
+     * @return Attribute
+     */
+//    protected function status(): Attribute
+//    {
+//        return Attribute::make(
+//            get: fn($value) => $value == 'active',
+//            set: fn($value) => 'active' ?? 'inactive'
+//        );
+//    }
+
 }
