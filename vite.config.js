7f6da040192b43b6a6d9b07b92512b15d76715df
amlxv <amirulfcso@gmail.com>
2023-06-14 11:41:39 +0800

    feat: alpinejs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/alpinejs@3.12.1.js'],
             refresh: true,
         }),
     ],
